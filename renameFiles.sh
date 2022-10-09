df0fce4a7b11dcff38e074715ff9783f5a9ebe19
João Batista <joao42lbatista@gmail.com>
2022-10-09 15:47:04 -0300

    dlPVANetAulas.sh, dlPVANetPraticas.sh, and renameFiles.sh - up set

--- renameFiles.sh
+++ renameFiles.sh
@@ -22,12 +22,12 @@
 #
 # Script: Rename the files .txt and .cpp to .c
 #
-# Last update: 08/10/2022
+# Last update: 09/10/2022
 #
 # Tip: Read the readme.md file
 #
-set -eE
-trap 'echo -e "\\n\\n${RED}Error at line $LINENO$NC - Command:\\n$RED$BASH_COMMAND\\n"' ERR
+set -eEuo pipefail
+trap 'echo -e "\\n\\n\e[1;31mError at line $LINENO\033[0m - Command:\\n\e[1;31m$BASH_COMMAND\033[0m\\n"' ERR
 
 folderToWork=$1
 if [ "$folderToWork" == '' ]; then
